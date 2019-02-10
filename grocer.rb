dff4e87b00f8eb6125c029d7d65b939a62314f94
Adam Gordon <adamjgordon24@gmail.com>
2019-02-10 21:59:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ def apply_coupons(cart, coupons)
         #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
       
-      hash[item][:count] -= coupon_hash[:num]
+      hash[item][:count] -= coupon[:num]
     end
   end
   hash
