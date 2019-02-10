03dfb4b59264654285fb4547f4ba8747b754a538
Adam Gordon <adamjgordon24@gmail.com>
2019-02-10 22:01:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
     # add coupon to cart
     item = coupon[:item]
     if !cart[item].nil? && cart[item][:count] >= coupon[:num]
-      temp = {"#{item} W/COUPON" => {
+      new_hash = {"#{item} W/COUPON" => {
         :price => coupon[:cost],
         :clearance => hash[item][:clearance],
         :count => 1
