37032a483cb0a0163033d92a1819f642c32604e9
Adam Gordon <adamjgordon24@gmail.com>
2019-02-10 22:30:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,10 +53,10 @@ def checkout(items, coupons)
   cart2 = apply_clearance(cart1)
   total = 0
   cart2.each do |name, price_hash|
-    total += price_hash[:price] * price_hash[:count]
+    sum += price_hash[:price] * price_hash[:count]
   end
-  if total > 100
-    total = total * (0.9)
+  if sum > 100
+     sum = sum * (0.9)
   end
-  total
+  sum
 end
