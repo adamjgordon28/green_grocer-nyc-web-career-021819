69093e8b81b73d98dcfbb0b669aed33bae56b48d
Adam Gordon <adamjgordon24@gmail.com>
2019-02-10 22:36:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,7 @@ def checkout(items, coupons)
   cart3 = apply_clearance(cart2)
   sum = 0
   cart3.each do |name, details|
-    sum += price_hash[:price] * price_hash[:count]
+    sum += details[:price] * price_hash[:count]
   end
   
   if sum > 100
